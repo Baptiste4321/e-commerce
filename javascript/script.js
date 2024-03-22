f149f458ee0be765792d450191d87aef188d6092
Kingfor118 <150907799+Kingfor118@users.noreply.github.com>
2024-03-22 18:28:30 +0100

    Acceuil

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,22 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slideimage");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
