f18e2b43e32cdc69a3aadd359ffc7af9a4c1319b
baptiste4321 <baptiste.chesnot@edu.ece.fr>
2024-03-23 23:21:48 +0100

    grosse maj

--- javascript/script1.js
+++ javascript/script1.js
@@ -4,3 +4,17 @@ const navLinks = document.querySelector(".nav-links")
 menuHamburger.addEventListener('click',()=>{
     navLinks.classList.toggle('mobile-menu')
 })
+var slideIndex = 1;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000);
+}
