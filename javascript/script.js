c76ef0d092410eb123d8d4b5e5b6a933e5be46b5
Kingfor118 <150907799+Kingfor118@users.noreply.github.com>
2024-03-23 15:26:31 +0100

    update

--- javascript/script.js
+++ javascript/script.js
@@ -8,20 +8,17 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = 0;
+carousel();
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
+function carousel() {
   var i;
-  var x = document.getElementsByClassName("slideimage");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
+  var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 1000); 
 }
