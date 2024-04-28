475b43ef6062012115171c5f5a7af54295da1dbe
Paul-Louis Songeur <paullouis.songeur@edu.ece.fr>
2024-04-28 17:47:20 +0200

    Caroussel [2.3] : boutons masqués

--- javascript/carroussel.js
+++ javascript/carroussel.js
@@ -1,7 +1,7 @@
 const initSlider = () => {
     const imageList = document.querySelector(".carroussel .liste-img")
     const slideButtons = document.querySelectorAll(".carroussel .boutton-defilement")
-
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth
     //
     slideButtons.forEach(button =>{
         button.addEventListener("click", () => {
@@ -11,6 +11,10 @@ const initSlider = () => {
         })
     })
 
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "block"
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "block"
+    }
     imageList.addEventListener("scroll", () => {
         handleSlideButtons()
     })
