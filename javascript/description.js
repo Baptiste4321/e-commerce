ecee45f8af3da57e645a0b10acde16ed9696c6ca
baptiste4321 <baptiste.chesnot@edu.ece.fr>
2024-04-29 12:06:40 +0200

    creation de description

---
+++ javascript/description.js
@@ -0,0 +1,8 @@
+// Définition de la variable contenant le chemin de l'image
+var imagePath = "image/image/3.jpg";
+
+// Récupération de l'élément image
+var imageElement = document.getElementById("add_img");
+
+// Attribution du chemin de l'image à la propriété src de l'élément image
+imageElement.src = imagePath;
